37c62999551bd4e3724de51a55b127d89f50079b
AlexSuarez97 <166262535+AlexSuarez97@users.noreply.github.com>
2025-05-11 09:09:36 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,69 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            char [][] board = {{'+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+'},
+                    {'|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'},
+                    {'+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' },
+                    {'|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'},
+                    {'+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+'},
+                    {'|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'},
+                    {'+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+'}};
+            printBoard(board);
+            Scanner input = new Scanner(System.in);
+        System.out.println("Please make your move by entering (1 - 9) ");
+           int position = input.nextInt();
+
+        System.out.println(position);
+
+        switch (position) {
+            case 1:
+                board [0][0] = 'X';
+                break;
+            case 2:
+                board [0][2] = 'X';
+                break;
+            case 3:
+                board [0][4] = 'X';
+                break;
+            case 4:
+                board [2][0] = 'X';
+                break;
+            case 5:
+                board [2][2] = 'X';
+                break;
+            case 6:
+                board [2][4] = 'X';
+                break;
+            case 7:
+                board [4][0] = 'X';
+                break;
+            case 8:
+                board [4][2] = 'X';
+                break;
+            case 9:
+                board [4][4] = 'X';
+                break;
+
+        }
+        printBoard(board);
+
+
+
+
     }
+
+    public static void printBoard(char[][] board) {
+        for (char [] row : board) {
+            for (char c : row){
+                System.out.print(c + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
 }
+
+
+
